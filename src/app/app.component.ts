2dff2013f1c16dd1d7357e3e36c9213ecf0db102
Guilherme Gonçalves <guilhermemarcosgoncalves14@gmail.com>
2022-07-12 12:04:31 -0300

    Alteração no titulo da pagina e do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'curso-tour-of-heros-frontend-angular';
+  title = 'Tour of Heros';
 }
