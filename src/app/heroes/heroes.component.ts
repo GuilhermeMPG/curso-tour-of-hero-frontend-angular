9432b7eb37a715c9a3be15619eb1ddbccce4e284
Guilherme Gonçalves <guilhermemarcosgoncalves14@gmail.com>
2022-07-14 13:36:34 -0300

    Lista de herois e funcinalidades do angular

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,19 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Hero } from '../hero.model';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  styleUrls: ['./heroes.component.scss'],
 })
-export class HeroesComponent implements OnInit {
-  hero : Hero = {
+export class HeroesComponent {
+  hero: Hero = {
     id: 1,
-    name: 'Batman',};
-
-  constructor() { }
-
-  ngOnInit(): void {
+    name: 'Batman',
+  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+  selectOn(hero: Hero): void {
+    this.selectedHero = hero;
   }
-
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero.model';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Batman' },
+  { id: 2, name: 'Naruto' },
+  { id: 3, name: 'Chaves' },
+  { id: 4, name: 'Capitão America' },
+  { id: 5, name: 'KickAss' },
+  { id: 6, name: 'Deadpool' },
+  { id: 7, name: 'Luffy' },
+  { id: 8, name: 'Jinx' },
+  { id: 9, name: 'Tarzan' },
+  { id: 10, name: 'Zoro' },
+];
